4caac272465c68bd93852252047ef81ad85a2306
hiroki_ostu <hiroki.ootsu.50054@gmail.com>
2017-12-25 15:52:20 +0900

    リンクなどを変更

--- web/enrollment.php
+++ web/enrollment.php
@@ -5,8 +5,6 @@
 <title>HAL学生管理システム|在籍確認画面</title>
 <link type="text/css" rel="stylesheet" href="css/reset/html5reset-1.6.1.css" />
 <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
-<link type="text/css" rel="stylesheet" href="css/themes/default.css">
-<link type="text/css" rel="stylesheet" href="css/themes/default.date.css">
 <link type="text/css" rel="stylesheet" href="css/materialize.min.css" />
 <link type="text/css" rel="stylesheet" href="css/desin/desin_format.css" />
 <link type="text/css" rel="stylesheet" href="css/desin/desin_enrollment.css" />
@@ -20,11 +18,11 @@
     </div>
     <div id="menu">
       <ul>
-        <li class="menubar"><a href="home.php"><img src="img/icon/ic_home_black_24dp_1x.png" width="24" height="24" alt="home"/>Home</a></li>
-        <li class="menubar"><a href="enrollment.php"><img src="img/icon/ic_airline_seat_recline_extra_black_24dp_1x.png" width="24" height="24" alt="座席確認"/>在籍確認</a></li>
-        <li class="menubar"><a href="events.php"><img src="img/icon/ic_event_note_black_24dp_1x.png" width="24" height="24" alt="イベント掲示板"/>イベント掲示板</a></li>
-        <li class="menubar"><a href="lost_article.php"><img src="img/icon/ic_live_help_black_24dp_1x.png" width="24" height="24" alt="忘れ物掲示板"/>忘れ物掲示板</a></li>
-        <li class="menubar"><a href="classroom.php"><img src="img/icon/ic_search_black_24dp_1x.png" width="24" height="24" alt="教室予約"/>教室検索・予約</a></li>
+        <li class="menubar"><a href="home.php"><img src="image/icon/ic_home_black_24dp_1x.png" width="24" height="24" alt="home"/>Home</a></li>
+        <li class="menubar"><a href="enrollment.php"><img src="image/icon/ic_airline_seat_recline_extra_black_24dp_1x.png" width="24" height="24" alt="座席確認"/>在籍確認</a></li>
+        <li class="menubar"><a href="events.php"><img src="image/icon/ic_event_note_black_24dp_1x.png" width="24" height="24" alt="イベント掲示板"/>イベント掲示板</a></li>
+        <li class="menubar"><a href="lost_article.php"><img src="image/icon/ic_live_help_black_24dp_1x.png" width="24" height="24" alt="忘れ物掲示板"/>忘れ物掲示板</a></li>
+        <li class="menubar"><a href="classroom.php"><img src="image/icon/ic_search_black_24dp_1x.png" width="24" height="24" alt="教室予約"/>教室検索・予約</a></li>
       </ul>
     </div>
   </div>
@@ -34,7 +32,7 @@
         <h2>HAL Students System</h2>
       </div>
       <div id="user">
-        <p><a href="mypagehome.php"><img src="img/icon/ic_person_black_24dp_1x.png" width="24" height="24" alt="アカウント"/>ohs50054:大津裕幹</a></p>
+        <p><a href="home.php"><img src="image/icon/ic_person_black_24dp_1x.png" width="24" height="24" alt="アカウント"/>ohs50054:大津裕幹</a></p>
       </div>
     </div><!-- end header -->
     <div id="seach" class="input-field">

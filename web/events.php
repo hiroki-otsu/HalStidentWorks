ba464c867f4eb3afd214d55020b34aa5b7377cfe
hiroki_ostu <hiroki.ootsu.50054@gmail.com>
2017-12-26 23:00:48 +0900

    <a>タグのリンクを変更や<img>タグを変更

--- web/events.php
+++ web/events.php
@@ -20,11 +20,11 @@
     </div>
     <div id="menu">
       <ul>
-        <li class="menubar"><a href="home.php"><img src="img/icon/ic_home_black_24dp_1x.png" width="24" height="24" alt="home"/>Home</a></li>
-        <li class="menubar"><a href="enrollment.php"><img src="img/icon/ic_airline_seat_recline_extra_black_24dp_1x.png" width="24" height="24" alt="座席確認"/>在籍確認</a></li>
-        <li class="menubar"><a href="events.php"><img src="img/icon/ic_event_note_black_24dp_1x.png" width="24" height="24" alt="イベント掲示板"/>イベント掲示板</a></li>
-        <li class="menubar"><a href="lost_article.php"><img src="img/icon/ic_live_help_black_24dp_1x.png" width="24" height="24" alt="忘れ物掲示板"/>忘れ物掲示板</a></li>
-        <li class="menubar"><a href="classroom.php"><img src="img/icon/ic_search_black_24dp_1x.png" width="24" height="24" alt="教室予約"/>教室検索・予約</a></li>
+        <li class="menubar"><a href="home.php"><img src="image/icon/ic_home_black_24dp_1x.png" width="24" height="24" alt="home"/>Home</a></li>
+        <li class="menubar"><a href="enrollment.php"><img src="image/icon/ic_airline_seat_recline_extra_black_24dp_1x.png" width="24" height="24" alt="座席確認"/>在籍確認</a></li>
+        <li class="menubar"><a href="events.php"><img src="image/icon/ic_event_note_black_24dp_1x.png" width="24" height="24" alt="イベント掲示板"/>イベント掲示板</a></li>
+        <li class="menubar"><a href="lost_article.php"><img src="image/icon/ic_live_help_black_24dp_1x.png" width="24" height="24" alt="忘れ物掲示板"/>忘れ物掲示板</a></li>
+        <li class="menubar"><a href="classroom.php"><img src="image/icon/ic_search_black_24dp_1x.png" width="24" height="24" alt="教室予約"/>教室検索・予約</a></li>
       </ul>
     </div>
   </div>
@@ -34,7 +34,7 @@
         <h2>HAL Students System</h2>
       </div>
       <div id="user">
-        <p><a href="mypagehome.php"><img src="img/icon/ic_person_black_24dp_1x.png" width="24" height="24" alt="アカウント"/>ohs50054:大津裕幹</a></p>
+        <p><a href="home.php"><img src="image/icon/ic_person_black_24dp_1x.png" width="24" height="24" alt="アカウント"/>ohs50054:大津裕幹</a></p>
       </div>
     </div><!-- end header -->
     <div id="eventpost">
@@ -67,7 +67,7 @@
               <td class="events_name">Kotlinを学ぼう!!</td>
               <td class="school_year">２年生・3年生</td>
               <td class="events_date">2017-11-12</td>
-              <td class="details_link"><a href="events_details.php"><img src="img/icon/ic_expand_more_black_24dp_1x.png" width="24" height="24" alt="詳細リンク"  /></a></td>
+              <td class="details_link"><a href="events_details.php"><img src="image/icon/ic_expand_more_black_24dp_1x.png" width="24" height="24" alt="詳細リンク"  /></a></td>
             </tr>
           </tbody>
         </table>

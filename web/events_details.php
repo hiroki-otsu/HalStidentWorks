2fa720afc8fb7159bbaf490a947df3212d127191
hiroki_ostu <hiroki.ootsu.50054@gmail.com>
2017-12-26 23:34:11 +0900

    イベント詳細pageを作成

---
+++ web/events_details.php
@@ -0,0 +1,99 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta charset="utf-8">
+<title>HAL学生管理システム|イベント詳細</title>
+<link type="text/css" rel="stylesheet" href="css/reset/html5reset-1.6.1.css" />
+<link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
+<link type="text/css" rel="stylesheet" href="css/themes/default.css">
+<link type="text/css" rel="stylesheet" href="css/themes/default.date.css">
+<link type="text/css" rel="stylesheet" href="css/materialize.min.css" />
+<link type="text/css" rel="stylesheet" href="css/desin/desin_format.css" />
+<link type="text/css" rel="stylesheet" href="css/desin/desin_events_details.css" />
+<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" />
+</head>
+<body>
+<div id="wrapper">
+  <div id="nav" class="z-depth-3">
+    <div id="logo">
+      <img src="" width="" height="" alt=""/>
+    </div>
+    <div id="menu">
+      <ul>
+        <li class="menubar"><a href="home.php"><img src="img/icon/ic_home_black_24dp_1x.png" width="24" height="24" alt="home"/>Home</a></li>
+        <li class="menubar"><a href="enrollment.php"><img src="img/icon/ic_airline_seat_recline_extra_black_24dp_1x.png" width="24" height="24" alt="座席確認"/>在籍確認</a></li>
+        <li class="menubar"><a href="events.php"><img src="img/icon/ic_event_note_black_24dp_1x.png" width="24" height="24" alt="イベント掲示板"/>イベント掲示板</a></li>
+        <li class="menubar"><a href="lost_article.php"><img src="img/icon/ic_live_help_black_24dp_1x.png" width="24" height="24" alt="忘れ物掲示板"/>忘れ物掲示板</a></li>
+        <li class="menubar"><a href="classroom.php"><img src="img/icon/ic_search_black_24dp_1x.png" width="24" height="24" alt="教室予約"/>教室検索・予約</a></li>
+      </ul>
+    </div>
+  </div>
+  <div id="contents">
+    <div id="header">
+      <div id="title">
+        <h2>HAL Students System</h2>
+      </div>
+      <div id="user">
+        <p><a href="mypagehome.php"><img src="img/icon/ic_person_black_24dp_1x.png" width="24" height="24" alt="アカウント"/>ohs50054:大津裕幹</a></p>
+      </div>
+    </div><!-- end header -->
+    <div id="event">
+      <div id="eventcomment">
+        <ul class="collection with-header" id="comment">
+          <li class="collection-header" ><h4>Kotlinを学ぼう!!</h4></li>
+          <li class="collection-item" id="item">
+            Android開発言語として注目されているKotlin、その活躍の場はAndroidだけじゃない！！！<br>
+            Kotlinはサーバーサイドでも、どこでも（デスクトップアプリやAltJSなどでも）使える言語です。エムスリーではAndroidアプリやサーバーサイドの開発言語としてKotlinを実践投入しています。<br>
+            「どこでもKotlin」では様々な分野におけるKotlinの話題について取り上げていきます<br>
+            第2回目となる今回のテーマは「サーバーサイドKotlin」 日本Kotlinユーザグループ代表 長澤太郎をはじめとするエムスリーのKotlinエンジニアが、サーバーサイドKotlinについて発表します。<br>
+            また、9/29には長澤太郎の新刊「Kotlin Webアプリケーション 新しいサーバサイドプログラミング（通称：黒ベコ本）」も出版されます！<br>
+            ※ 第1回目の様子はこちら<br>
+            開催日 2017/12/25<br>
+            投稿時間 00:00<br>
+            Android開発言語として注目されているKotlin、その活躍の場はAndroidだけじゃない！！！<br>
+            Kotlinはサーバーサイドでも、どこでも（デスクトップアプリやAltJSなどでも）使える言語です。エムスリーではAndroidアプリやサーバーサイドの開発言語としてKotlinを実践投入しています。<br>
+            「どこでもKotlin」では様々な分野におけるKotlinの話題について取り上げていきます<br>
+            第2回目となる今回のテーマは「サーバーサイドKotlin」 日本Kotlinユーザグループ代表 長澤太郎をはじめとするエムスリーのKotlinエンジニアが、サーバーサイドKotlinについて発表します。<br>
+            また、9/29には長澤太郎の新刊「Kotlin Webアプリケーション 新しいサーバサイドプログラミング（通称：黒ベコ本）」も出版されます！<br>
+            ※ 第1回目の様子はこちら<br>
+            開催日 2017/12/25<br>
+            投稿時間 00:00<br>
+            Android開発言語として注目されているKotlin、その活躍の場はAndroidだけじゃない！！！<br>
+            Kotlinはサーバーサイドでも、どこでも（デスクトップアプリやAltJSなどでも）使える言語です。エムスリーではAndroidアプリやサーバーサイドの開発言語としてKotlinを実践投入しています。<br>
+            「どこでもKotlin」では様々な分野におけるKotlinの話題について取り上げていきます<br>
+            第2回目となる今回のテーマは「サーバーサイドKotlin」 日本Kotlinユーザグループ代表 長澤太郎をはじめとするエムスリーのKotlinエンジニアが、サーバーサイドKotlinについて発表します。<br>
+            また、9/29には長澤太郎の新刊「Kotlin Webアプリケーション 新しいサーバサイドプログラミング（通称：黒ベコ本）」も出版されます！<br>
+            ※ 第1回目の様子はこちら<br>
+            開催日 2017/12/25<br>
+            投稿時間 00:00<br>
+            Android開発言語として注目されているKotlin、その活躍の場はAndroidだけじゃない！！！<br>
+            Kotlinはサーバーサイドでも、どこでも（デスクトップアプリやAltJSなどでも）使える言語です。エムスリーではAndroidアプリやサーバーサイドの開発言語としてKotlinを実践投入しています。<br>
+            「どこでもKotlin」では様々な分野におけるKotlinの話題について取り上げていきます<br>
+            第2回目となる今回のテーマは「サーバーサイドKotlin」 日本Kotlinユーザグループ代表 長澤太郎をはじめとするエムスリーのKotlinエンジニアが、サーバーサイドKotlinについて発表します。<br>
+            また、9/29には長澤太郎の新刊「Kotlin Webアプリケーション 新しいサーバサイドプログラミング（通称：黒ベコ本）」も出版されます！<br>
+            ※ 第1回目の様子はこちら<br>
+            開催日 2017/12/25<br>
+            投稿時間 00:00<br>
+          </li>
+        </ul>
+      </div>
+      <div id="eventpropriety">
+        <a class="waves-effect waves-light btn" id="btnjoin"><p>参加</p></a>
+        <a class="waves-effect waves-light btn" id="btnnotgoing"><p>不参加</p></a>
+      </div>
+    </div>
+    <div id="footer">
+      <footer>2017 HAL Students System</footer>
+    </div><!-- footer -->
+  </div><!-- end contents -->
+</div><!-- end wrapper -->
+<script type="text/javascript" src="jq/jquery-3.2.1.min.js"></script>
+<script type="text/javascript" src="js/check.js"></script>
+<script type="text/javascript" src="js/materialize.min.js"></script>
+<script type="text/javascript" src="js/legacy.js"></script>
+<script type="text/javascript" src="js/lang-ja.js"></script>
+<script type="text/javascript" src="js/app.js"></script>
+<script type="text/javascript" src="js/picker.js"></script>
+<script type="text/javascript" src="js/picker.date.js"></script>
+</body>
+</html>

e8177ab651cb8286ea7170442aaa68a69dfa6076
hiroki_ostu <hiroki.ootsu.50054@gmail.com>
2017-12-29 23:28:57 +0900

    index.phpを追加

--- web/index.php
+++ web/index.php
@@ -1,3 +1,54 @@
 <?php
 
-require '../booststrap.php';
+?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta charset="utf-8">
+<title>HAL学生管理システム|</title>
+<link type="text/css" rel="stylesheet" href="css/reset/html5reset-1.6.1.css" />
+<link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
+<link type="text/css" rel="stylesheet" href="css/materialize.min.css" />
+<link type="text/css" rel="stylesheet" href="css/design/design_index.css" />
+<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" />
+</head>
+<body>
+<div id="wrapper">
+    <div id="header">
+        <div id="title">
+            <h2>HAL Students System</h2>
+        </div>
+    </div><!-- end header -->
+    <div id="contents">
+        <div id="login-form">
+
+            <ul class="collection with-header">
+                <li class="collection-item">ログインフォーム</li>
+                <li class="collection-item">
+                    <form action="Login.php" method="post">
+                        <div class="input-field" id="login-student">
+                            <input id="user" type="text" name="student" value="" class="validate">
+                            <label for="user">ユーザID</label>
+                        </div>
+                        <div class="input-field" id="login-pass">
+                            <input id="pass" type="password" name="pass" value="" class="validate">
+                            <label for="pass">パスワード</label>
+                        </div>
+                        <button class="btn waves-effect waves-light" type="submit" name="action" id="btn-login">ログイン
+                            <i class="material-icons right">send</i>
+                        </button>
+                    </form>
+                </li>
+            </ul>
+        </div>
+    </div><!-- end contents -->
+    <div id="footer">
+        <footer>2017 HAL Students System</footer>
+    </div><!-- footer -->
+</div><!-- end wrapper -->
+
+<script type="text/javascript" src="jq/jquery-3.2.1.min.js"></script>
+<script type="text/javascript" src="js/materialize.min.js"></script>
+</body>
+</html>
+

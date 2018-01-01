de66def166a279422a18dff4124df26d0f488b1e
hiroki_ostu <hiroki.ootsu.50054@gmail.com>
2018-01-02 01:51:14 +0900

    cssのリンク修正やsessionクラスを追加

--- web/event_post.php
+++ web/event_post.php
@@ -1,3 +1,13 @@
+<?php
+//エラー表示
+error_reporting(E_ALL | E_STRICT);
+ini_set('display_errors','On');
+//外部ファイル読み込み
+require '../bootstrap.php';
+//インスタンス化
+$session = new Session();
+$access = new DataAccess();
+?>
 <!DOCTYPE html>
 <html lang="ja">
 <head>
@@ -8,8 +18,8 @@
 <link type="text/css" rel="stylesheet" href="css/themes/default.css">
 <link type="text/css" rel="stylesheet" href="css/themes/default.date.css">
 <link type="text/css" rel="stylesheet" href="css/materialize.min.css" />
-<link type="text/css" rel="stylesheet" href="css/desin/desin_format.css" />
-<link type="text/css" rel="stylesheet" href="css/desin/desin_events_post.css" />
+<link type="text/css" rel="stylesheet" href="css/design/design_format.css" />
+<link type="text/css" rel="stylesheet" href="css/design/design_events_post.css" />
 <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" />
 </head>
 <body>
@@ -20,11 +30,11 @@
     </div>
     <div id="menu">
       <ul>
-        <li class="menubar"><a href="home.php"><img src="img/icon/ic_home_black_24dp_1x.png" width="24" height="24" alt="home"/>Home</a></li>
-        <li class="menubar"><a href="enrollment.php"><img src="img/icon/ic_airline_seat_recline_extra_black_24dp_1x.png" width="24" height="24" alt="座席確認"/>在籍確認</a></li>
-        <li class="menubar"><a href="events.php"><img src="img/icon/ic_event_note_black_24dp_1x.png" width="24" height="24" alt="イベント掲示板"/>イベント掲示板</a></li>
-        <li class="menubar"><a href="lost_article.php"><img src="img/icon/ic_live_help_black_24dp_1x.png" width="24" height="24" alt="忘れ物掲示板"/>忘れ物掲示板</a></li>
-        <li class="menubar"><a href="classroom.php"><img src="img/icon/ic_search_black_24dp_1x.png" width="24" height="24" alt="教室予約"/>教室検索・予約</a></li>
+        <li class="menubar"><a href="home.php"><img src="image/icon/ic_home_black_24dp_1x.png" width="24" height="24" alt="home"/>Home</a></li>
+        <li class="menubar"><a href="enrollment.php"><img src="image/icon/ic_airline_seat_recline_extra_black_24dp_1x.png" width="24" height="24" alt="座席確認"/>在籍確認</a></li>
+        <li class="menubar"><a href="events.php"><img src="image/icon/ic_event_note_black_24dp_1x.png" width="24" height="24" alt="イベント掲示板"/>イベント掲示板</a></li>
+        <li class="menubar"><a href="lost_article.php"><img src="image/icon/ic_live_help_black_24dp_1x.png" width="24" height="24" alt="忘れ物掲示板"/>忘れ物掲示板</a></li>
+        <li class="menubar"><a href="classroom.php"><img src="image/icon/ic_search_black_24dp_1x.png" width="24" height="24" alt="教室予約"/>教室検索・予約</a></li>
       </ul>
     </div>
   </div>
@@ -34,7 +44,7 @@
         <h2>HAL Students System</h2>
       </div>
       <div id="user">
-        <p><a href="mypagehome.php"><img src="img/icon/ic_person_black_24dp_1x.png" width="24" height="24" alt="アカウント"/>ohs50054:大津裕幹</a></p>
+          <p><a href="home.php"><img src="image/icon/ic_person_black_24dp_1x.png" width="24" height="24" alt="アカウント"/><?php echo $session->get('ohs50054')?></a></p>
       </div>
     </div><!-- end header -->
     <div id="event">

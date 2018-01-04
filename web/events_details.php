df04a368e0e806dfff15dd6429782cb419210c54
hiroki_ostu <hiroki.ootsu.50054@gmail.com>
2018-01-04 16:35:03 +0900

    getEventDetails($event_code);を追加

--- web/events_details.php
+++ web/events_details.php
@@ -50,41 +50,16 @@ $access = new DataAccess();
     <div id="event">
       <div id="eventcomment">
         <ul class="collection with-header" id="comment">
-          <li class="collection-header" ><h4>Kotlinを学ぼう!!</h4></li>
-          <li class="collection-item" id="item">
-            Android開発言語として注目されているKotlin、その活躍の場はAndroidだけじゃない！！！<br>
-            Kotlinはサーバーサイドでも、どこでも（デスクトップアプリやAltJSなどでも）使える言語です。エムスリーではAndroidアプリやサーバーサイドの開発言語としてKotlinを実践投入しています。<br>
-            「どこでもKotlin」では様々な分野におけるKotlinの話題について取り上げていきます<br>
-            第2回目となる今回のテーマは「サーバーサイドKotlin」 日本Kotlinユーザグループ代表 長澤太郎をはじめとするエムスリーのKotlinエンジニアが、サーバーサイドKotlinについて発表します。<br>
-            また、9/29には長澤太郎の新刊「Kotlin Webアプリケーション 新しいサーバサイドプログラミング（通称：黒ベコ本）」も出版されます！<br>
-            ※ 第1回目の様子はこちら<br>
-            開催日 2017/12/25<br>
-            投稿時間 00:00<br>
-            Android開発言語として注目されているKotlin、その活躍の場はAndroidだけじゃない！！！<br>
-            Kotlinはサーバーサイドでも、どこでも（デスクトップアプリやAltJSなどでも）使える言語です。エムスリーではAndroidアプリやサーバーサイドの開発言語としてKotlinを実践投入しています。<br>
-            「どこでもKotlin」では様々な分野におけるKotlinの話題について取り上げていきます<br>
-            第2回目となる今回のテーマは「サーバーサイドKotlin」 日本Kotlinユーザグループ代表 長澤太郎をはじめとするエムスリーのKotlinエンジニアが、サーバーサイドKotlinについて発表します。<br>
-            また、9/29には長澤太郎の新刊「Kotlin Webアプリケーション 新しいサーバサイドプログラミング（通称：黒ベコ本）」も出版されます！<br>
-            ※ 第1回目の様子はこちら<br>
-            開催日 2017/12/25<br>
-            投稿時間 00:00<br>
-            Android開発言語として注目されているKotlin、その活躍の場はAndroidだけじゃない！！！<br>
-            Kotlinはサーバーサイドでも、どこでも（デスクトップアプリやAltJSなどでも）使える言語です。エムスリーではAndroidアプリやサーバーサイドの開発言語としてKotlinを実践投入しています。<br>
-            「どこでもKotlin」では様々な分野におけるKotlinの話題について取り上げていきます<br>
-            第2回目となる今回のテーマは「サーバーサイドKotlin」 日本Kotlinユーザグループ代表 長澤太郎をはじめとするエムスリーのKotlinエンジニアが、サーバーサイドKotlinについて発表します。<br>
-            また、9/29には長澤太郎の新刊「Kotlin Webアプリケーション 新しいサーバサイドプログラミング（通称：黒ベコ本）」も出版されます！<br>
-            ※ 第1回目の様子はこちら<br>
-            開催日 2017/12/25<br>
-            投稿時間 00:00<br>
-            Android開発言語として注目されているKotlin、その活躍の場はAndroidだけじゃない！！！<br>
-            Kotlinはサーバーサイドでも、どこでも（デスクトップアプリやAltJSなどでも）使える言語です。エムスリーではAndroidアプリやサーバーサイドの開発言語としてKotlinを実践投入しています。<br>
-            「どこでもKotlin」では様々な分野におけるKotlinの話題について取り上げていきます<br>
-            第2回目となる今回のテーマは「サーバーサイドKotlin」 日本Kotlinユーザグループ代表 長澤太郎をはじめとするエムスリーのKotlinエンジニアが、サーバーサイドKotlinについて発表します。<br>
-            また、9/29には長澤太郎の新刊「Kotlin Webアプリケーション 新しいサーバサイドプログラミング（通称：黒ベコ本）」も出版されます！<br>
-            ※ 第1回目の様子はこちら<br>
-            開催日 2017/12/25<br>
-            投稿時間 00:00<br>
-          </li>
+            <?php
+            $event_code=$_GET['event'];
+            $dataList =$access -> getEventDetails($event_code);
+                foreach ($dataList as $value){
+                    echo '<li class="collection-header" ><h4>'.$value['events_title'].'</h4></li>';
+                    echo '<li class="collection-item" id="item">';
+                    echo $value['events_contents'].PHP_EOL;
+                    echo '</li>';
+                }
+            ?>
         </ul>
       </div>
       <div id="eventpropriety">

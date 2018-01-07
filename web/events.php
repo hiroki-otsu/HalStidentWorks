b87456bc8ccf83d0521e004c2e1d338e6ba75179
hiroki_ostu <hiroki.ootsu.50054@gmail.com>
2018-01-07 20:49:40 +0900

    htmlタグの修正

--- web/events.php
+++ web/events.php
@@ -12,85 +12,82 @@ $access = new DataAccess();
 <html lang="ja">
 <head>
 <meta charset="utf-8">
-<title>HAL学生管理システム|イベント掲示板</title>
-<link type="text/css" rel="stylesheet" href="css/reset/html5reset-1.6.1.css" />
-<link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
-<link type="text/css" rel="stylesheet" href="css/materialize.min.css" />
-<link type="text/css" rel="stylesheet" href="css/design/design_format.css" />
-<link type="text/css" rel="stylesheet" href="css/design/design_events.css" />
-<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" />
+    <title>HAL学生管理システム|イベント掲示板</title>
+    <link type="text/css" rel="stylesheet" href="css/reset/html5reset-1.6.1.css" />
+    <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
+    <link type="text/css" rel="stylesheet" href="css/materialize.min.css" />
+    <link type="text/css" rel="stylesheet" href="css/design/design_format.css" />
+    <link type="text/css" rel="stylesheet" href="css/design/design_events.css" />
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" />
 </head>
 <body>
 <div id="wrapper">
-  <div id="nav" class="z-depth-3">
-    <div id="logo">
-      <img src="" width="" height="" alt=""/>
-    </div>
-    <div id="menu">
-      <ul>
-        <li class="menubar"><a href="home.php"><img src="image/icon/ic_home_black_24dp_1x.png" width="24" height="24" alt="home"/>Home</a></li>
-        <li class="menubar"><a href="enrollment.php"><img src="image/icon/ic_airline_seat_recline_extra_black_24dp_1x.png" width="24" height="24" alt="座席確認"/>在籍確認</a></li>
-        <li class="menubar"><a href="events.php"><img src="image/icon/ic_event_note_black_24dp_1x.png" width="24" height="24" alt="イベント掲示板"/>イベント掲示板</a></li>
-        <li class="menubar"><a href="lost_article.php"><img src="image/icon/ic_live_help_black_24dp_1x.png" width="24" height="24" alt="忘れ物掲示板"/>忘れ物掲示板</a></li>
-        <li class="menubar"><a href="classroom.php"><img src="image/icon/ic_search_black_24dp_1x.png" width="24" height="24" alt="教室予約"/>教室検索・予約</a></li>
-      </ul>
-    </div>
-  </div>
-  <div id="contents">
-    <div id="header">
-      <div id="title">
-        <h2>HAL Students System</h2>
-      </div>
-      <div id="user">
-          <p><a href="home.php"><img src="image/icon/ic_person_black_24dp_1x.png" width="24" height="24" alt="アカウント"/><?php echo $session->get('ohs50054')?></a></p>
-      </div>
-    </div><!-- end header -->
-    <div id="eventpost">
-      <a href="event_post.php"class="waves-effect waves-light btn-large">イベント投稿へ</a>
-    </div>
-    <div id="events">
-      <div id="option">
-        <div class="input-field col s6">
-          <select multiple>
-            <option value="" disabled selected>Choose your optio</option>
-            <option value="1">1年生</option>
-            <option value="2">2年生</option>
-            <option value="3">3年生</option>
-          </select>
-          <label>対象学年絞り込み</label>
+    <div id="nav" class="z-depth-3">
+        <div id="logo">
+            <img src="" width="" height="" alt=""/>
         </div>
-      </div>
-      <div id="list">
-        <table class="highlight">
-          <thead>
-            <tr>
-              <th class="events_name">イベント名</th>
-              <th class="school_year">対象学年</th>
-              <th class="events_date">開催日</th>
-              <th class="details_link">詳細</th>
-            </tr>
-          </thead>
-          <tbody>
-              <?php print $access -> getEventsInformation(); ?>
-          </tbody>
-        </table>
-      </div>
-      <div id="pageination">
-        <ul class="pagination">
-          <li class="disabled"><a href="#!"><i class="material-icons">chevron_left</i></a></li>
-          <li class="active"><a href="#!">1</a></li>
-          <li class="waves-effect"><a href="#!">2</a></li>
-          <li class="waves-effect"><a href="#!">3</a></li>
-          <li class="waves-effect"><a href="#!">4</a></li>
-          <li class="waves-effect"><a href="#!">5</a></li>
-          <li class="waves-effect"><a href="#!"><i class="material-icons">chevron_right</i></a></li>
-        </ul>
-      </div>
-    <div id="footer">
-      <footer>2017 HAL Students System</footer>
-    </div><!-- footer -->
-  </div><!-- end contents -->
-</div><!-- end wrapper -->
+        <div id="menu">
+            <ul>
+                <li class="menubar"><a href="home.php"><img src="image/icon/ic_home_black_24dp_1x.png" width="24" height="24" alt="home"/>Home</a></li>
+                <li class="menubar"><a href="enrollment.php"><img src="image/icon/ic_airline_seat_recline_extra_black_24dp_1x.png" width="24" height="24" alt="座席確認"/>在籍確認</a></li>
+                <li class="menubar"><a href="events.php"><img src="image/icon/ic_event_note_black_24dp_1x.png" width="24" height="24" alt="イベント掲示板"/>イベント掲示板</a></li>
+                <li class="menubar"><a href="lost_article.php"><img src="image/icon/ic_live_help_black_24dp_1x.png" width="24" height="24" alt="忘れ物掲示板"/>忘れ物掲示板</a></li>
+                <li class="menubar"><a href="classroom.php"><img src="image/icon/ic_search_black_24dp_1x.png" width="24" height="24" alt="教室予約"/>教室検索・予約</a></li>
+            </ul>
+        </div><!-- menu -->
+    </div><!-- nav -->
+    <div id="contents">
+        <div id="header">
+            <div id="title">
+                <h2>HAL Students System</h2>
+            </div>
+            <div id="user">
+                <p><a href="home.php"><img src="image/icon/ic_person_black_24dp_1x.png" width="24" height="24" alt="アカウント"/><?php echo $session->get('ohs50054')?></a></p>
+            </div>
+        </div><!-- end header -->
+        <div id="eventpost">
+            <a href="event_post.php"class="waves-effect waves-light btn-large">イベント投稿へ</a>
+        </div>
+        <div class="input-field" id="option">
+            <select multiple>
+                <option value="" disabled selected>Choose your optio</option>
+                <option value="1">1年生</option>
+                <option value="2">2年生</option>
+                <option value="3">3年生</option>
+            </select>
+            <label>対象学年絞り込み</label>
+        </div>
+        <div id="events">
+            <table class="highlight" id="list">
+                <thead>
+                <tr>
+                    <th class="events_name">イベント名</th>
+                    <th class="school_year">対象学年</th>
+                    <th class="events_date">開催日</th>
+                    <th class="details_link">詳細</th>
+                </tr>
+                </thead>
+                <tbody>
+                    <?php print $access -> getEventsInformation(); ?>
+                </tbody>
+            </table>
+        </div>
+        <div id="page">
+            <ul class="pagination">
+                <li class="disabled"><a href="#!"><i class="material-icons">chevron_left</i></a></li>
+                <li class="active"><a href="#!">1</a></li>
+                <li class="waves-effect"><a href="#!">2</a></li>
+                <li class="waves-effect"><a href="#!">3</a></li>
+                <li class="waves-effect"><a href="#!">4</a></li>
+                <li class="waves-effect"><a href="#!">5</a></li>
+                <li class="waves-effect"><a href="#!"><i class="material-icons">chevron_right</i></a></li>
+            </ul>
+        </div>
+        <div id="footer">
+            <footer>2017 HAL Students System</footer>
+        </div><!-- footer -->
+    </div>
+</div><!-- wrapper -->
 <script type="text/javascript" src="jq/jquery-3.2.1.min.js"></script>
 <script type="text/javascript" src="js/check.js"></script>
 <script type="text/javascript" src="js/materialize.min.js"></script>

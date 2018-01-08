54a9ec5ae0ed1a90241297577a5c2ecec2b1bdf3
hiroki_ostu <hiroki.ootsu.50054@gmail.com>
2018-01-09 02:31:22 +0900

    RequestクラスとDataAccessクラスとErrorsクラスを追加

--- web/password_change.php
+++ web/password_change.php
@@ -5,3 +5,19 @@
  * Date: 2018/01/09
  * Time: 1:50
  */
+//エラー表示
+error_reporting(E_ALL | E_STRICT);
+ini_set('display_errors','On');
+//外部ファイル読み込み
+require '../bootstrap.php';
+//インスタンス化
+$request = new Request();
+$access = new DataAccess();
+$error = new Errors();
+
+if($request->isPost()){
+    $request->getPost('current');
+    $request->getPost('new');
+    $request->getPost('confirmation');
+
+}

22d587fbac3ea294d6ed6e9000e3c916f7a27642
hiroki_ostu <hiroki.ootsu.50054@gmail.com>
2018-01-12 01:19:49 +0900

    イベント投稿pageを作成

--- web/event_post.php
+++ web/event_post.php
@@ -24,64 +24,70 @@ $access = new DataAccess();
 </head>
 <body>
 <div id="wrapper">
-  <div id="nav" class="z-depth-3">
-    <div id="logo">
-      <img src="" width="" height="" alt=""/>
+    <div id="nav" class="z-depth-3">
+        <div id="logo">
+            <img src="" width="" height="" alt=""/>
+        </div>
+        <div id="menu">
+            <ul>
+                <li class="menubar"><a href="home.php"><img src="image/icon/ic_home_black_24dp_1x.png" width="24" height="24" alt="home"/>Home</a></li>
+                <li class="menubar"><a href="enrollment.php"><img src="image/icon/ic_airline_seat_recline_extra_black_24dp_1x.png" width="24" height="24" alt="座席確認"/>在籍確認</a></li>
+                <li class="menubar"><a href="events.php"><img src="image/icon/ic_event_note_black_24dp_1x.png" width="24" height="24" alt="イベント掲示板"/>イベント掲示板</a></li>
+                <li class="menubar"><a href="lost_article.php"><img src="image/icon/ic_live_help_black_24dp_1x.png" width="24" height="24" alt="忘れ物掲示板"/>忘れ物掲示板</a></li>
+                <li class="menubar"><a href="classroom.php"><img src="image/icon/ic_search_black_24dp_1x.png" width="24" height="24" alt="教室予約"/>教室検索・予約</a></li>
+            </ul>
+        </div>
     </div>
-    <div id="menu">
-      <ul>
-        <li class="menubar"><a href="home.php"><img src="image/icon/ic_home_black_24dp_1x.png" width="24" height="24" alt="home"/>Home</a></li>
-        <li class="menubar"><a href="enrollment.php"><img src="image/icon/ic_airline_seat_recline_extra_black_24dp_1x.png" width="24" height="24" alt="座席確認"/>在籍確認</a></li>
-        <li class="menubar"><a href="events.php"><img src="image/icon/ic_event_note_black_24dp_1x.png" width="24" height="24" alt="イベント掲示板"/>イベント掲示板</a></li>
-        <li class="menubar"><a href="lost_article.php"><img src="image/icon/ic_live_help_black_24dp_1x.png" width="24" height="24" alt="忘れ物掲示板"/>忘れ物掲示板</a></li>
-        <li class="menubar"><a href="classroom.php"><img src="image/icon/ic_search_black_24dp_1x.png" width="24" height="24" alt="教室予約"/>教室検索・予約</a></li>
-      </ul>
-    </div>
-  </div>
-  <div id="contents">
-    <div id="header">
-      <div id="title">
-        <h2>HAL Students System</h2>
-      </div>
-      <div id="user">
-          <p><a href="home.php"><img src="image/icon/ic_person_black_24dp_1x.png" width="24" height="24" alt="アカウント"/><?php echo $session->get('ohs50054')?></a></p>
-      </div>
-    </div><!-- end header -->
-    <div id="event">
-      <form class="" action="index.html" method="post">
-        <div class="input-field col s6" id="eventname">
-          <input placeholder="Placeholder" id="first_name" type="text" class="validate">
-          <label for="Event_name">イベント名</label>
-        </div>
+    <div id="contents">
+        <div id="header">
+            <div id="title">
+                <h2>HAL Students System</h2>
+            </div>
+            <div id="user">
+                <p><a href="home.php"><img src="image/icon/ic_person_black_24dp_1x.png" width="24" height="24" alt="アカウント"/><?php echo $session->get('ohs50054')?></a></p>
+            </div>
+        </div><!-- end header -->
+        <div id="event">
+          <form action="event_" method="get">
 
-        <div class="input-field col s6" id="eventdate">
-          <input placeholder="Placeholder" id="date" type="text" class="validate datepicker" >
-          <label for="events_date">開催日</label>
-        </div>
+              <div class="input-field" id="even-title">
+                  <input id="event-name" type="text" class="validate">
+                  <label for="event-name">イベント名</label>
+              </div>
+              <div class="input-field" id="category">
+                  <select>
+                      <option value="" disabled selected>Choose your option</option>
+                      <option value="1">ゲーム</option>
+                      <option value="3">デザイン</option>
+                      <option value="4">ミュージック</option>
+                      <option value="5">IT</option>
+                      <option value="6">その他</option>
+                  </select>
+                  <label>Category Select</label>
+              </div>
+              <div class="input-field" id="event-date">
+                  <input id="events-date" type="text" class="datepicker">
+                  <label for="events-date">開催日</label>
+              </div>
 
-        <div class="input-field col s6">
-        <select multiple>
-          <option value="" disabled selected>Choose your option</option>
-          <option value="1">1年生</option>
-          <option value="2">2年生</option>
-          <option value="3">3年生</option>
-        </select>
-        <label>対象学年</label>
-        </div>
-        <div class="input-field col s6">
-          <textarea id="icon_prefix2" class="materialize-textarea"></textarea>
-          <label for="icon_prefix2">Comment</label>
-        </div>
+              <div id="event-content">
+                  <div class="input-field" id="event-contents">
+                      <textarea class="materialize-textarea"></textarea>
+                      <label for="contents">Comment</label>
+                  </div>
+              </div>
 
-        <button class="btn waves-effect waves-light" type="submit" name="action">Submit
-           <i class="material-icons right">send</i>
-        </button>
-      </form>
-    </div>
-    <div id="footer">
-      <footer>2017 HAL Students System</footer>
-    </div><!-- footer -->
-  </div><!-- end contents -->
+              <div id="send">
+                  <button class="btn waves-effect waves-light"  id="btn-send" type="submit" name="action">イベントを投稿
+                      <i class="material-icons right">send</i>
+                  </button>
+              </div>
+          </form>
+        </div>
+        <div id="footer">
+            <footer>2017 HAL Students System</footer>
+        </div><!-- footer -->
+    </div><!-- end contents -->
 </div><!-- end wrapper -->
 <script type="text/javascript" src="jq/jquery-3.2.1.min.js"></script>
 <script type="text/javascript" src="js/check.js"></script>

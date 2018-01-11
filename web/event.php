e271a36309f781715356f8da3091cdd19892877e
hiroki_ostu <hiroki.ootsu.50054@gmail.com>
2018-01-12 02:19:42 +0900

    使用するクラスを記述

--- web/event.php
+++ web/event.php
@@ -5,3 +5,15 @@
  * Date: 2018/01/12
  * Time: 1:32
  */
+//エラー表示
+error_reporting(E_ALL | E_STRICT);
+ini_set('display_errors','On');
+//外部ファイル読み込み
+require '../bootstrap.php';
+//インスタンス化
+$session = new Session();
+$request = new Request();
+$access = new DataAccess();
+$error = new Errors();
+
+
---
+++ web/lost_article.php
@@ -0,0 +1,12 @@
+<?php
+//エラー表示
+error_reporting(E_ALL | E_STRICT);
+ini_set('display_errors','On');
+//外部ファイル読み込み
+require '../bootstrap.php';
+//インスタンス化
+$session = new Session();
+$request = new Request();
+$access = new DataAccess();
+$error = new Errors();
+

55e3ae3c6f1f04bc9184792377418e7ac0b425ac
hiroki_ostu <hiroki.ootsu.50054@gmail.com>
2018-01-13 00:58:09 +0900

    忘れ物投稿の処理を記述

--- web/lost_article.php
+++ web/lost_article.php
@@ -10,3 +10,61 @@ $request = new Request();
 $access = new DataAccess();
 $error = new Errors();
 
+if ($request-> isPost()) {
+    $title=$request->getPost('title');
+    $category=$request->getPost('category');
+    $file=$_FILES["image"];
+    $comment=$request->getPost('comment');
+    $flg=true;
+    if(empty($title)){
+        $error->setErrors('タイトルが入力されていません');
+        $flg=false;
+        echo 'タイトル';
+    }
+    if(empty($category)){
+        $error->setErrors('カテゴリーが選択されていません');
+        $flg=false;
+        echo 'カテゴリー';
+
+    }
+    if(empty($file)){
+        $error->setErrors('ファイルが選択されていません');
+        $flg=false;
+        echo 'ファイル';
+    }
+    if(empty($comment)){
+        $error->setErrors('コメントが入力されていません');
+        $flg=false;
+        echo 'コメント';
+    }
+    if ($flg){
+        $tmp_name = $file['tmp_name']; // 一時ファイルのパス
+        $tmp_size = getimagesize($tmp_name); // 一時ファイルの情報を取得
+        $img = $extension = null;
+        switch ($tmp_size[2]) { // 画像の種類を判別
+            case 1 : // GIF
+                $img = imageCreateFromGIF($tmp_name);
+                $extension = 'gif';
+                break;
+            case 2 : // JPEG
+                $img = imageCreateFromJPEG($tmp_name);
+                $extension = 'jpg';
+                break;
+            case 3 : // PNG
+                $img = imageCreateFromPNG($tmp_name);
+                $extension = 'png';
+                break;
+            default : break;
+        }
+        $save_filename = date('YmdHis');
+        $imageDirectory = 'image/lostArticle/' .$save_filename.'.'.$extension;
+        if(move_uploaded_file($_FILES['image']['tmp_name'], $imageDirectory)){
+            $msg = $imageDirectory. 'のアップロードに成功しました';
+            $access->setLostArticle($title,$category,$comment,$imageDirectory,$student);
+            header();
+        }else {
+            $msg = 'アップロードに失敗しました';
+        }
+        echo $msg;
+    }
+}

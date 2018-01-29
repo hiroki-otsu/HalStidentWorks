6729468c89782ace1fb1c30452c493cfafb3a959
hiroki_ostu <hiroki.ootsu.50054@gmail.com>
2018-01-30 01:21:03 +0900

    ページング機能を追加

--- web/lost_article_list.php
+++ web/lost_article_list.php
@@ -7,18 +7,26 @@ require '../bootstrap.php';
 //インスタンス化
 $session = new Session();
 $access = new DataAccess();
+define('LOST_ARTICLE_PAGE',6);
+define('LOST_ARTICLE','lostarticle');
+
+if(isset($_GET['page'])){
+    preg_match('/^[1-9][0-9]*$/',$_GET['page']);
+    $page = (int)$_GET['page'];
+}
+else{
+    $page =1;
+}
+$total = $access->getCountDate(LOST_ARTICLE);
+$totalPage = ceil($total / LOST_ARTICLE_PAGE);
 ?>
 <!DOCTYPE html>
 <html lang="ja">
 <head>
     <meta charset="utf-8">
-    <title>HAL学生管理システム|忘れ物掲示板</title>
+    <title>拾得物</title>
     <link type="text/css" rel="stylesheet" href="css/reset/html5reset-1.6.1.css" />
     <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
-    <link type="text/css" rel="stylesheet" href="css/perfect-scrollbar.css" />
-    <link type="text/css" rel="stylesheet" href="css/themes/default.css">
-    <link type="text/css" rel="stylesheet" href="css/themes/default.date.css">
-    <link type="text/css" rel="stylesheet" href="css/materialize.min.css" />
     <link type="text/css" rel="stylesheet" href="css/design/design_format.css" />
     <link type="text/css" rel="stylesheet" href="css/design/design_lost_article_list.css" />
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" />
@@ -55,104 +63,70 @@ $access = new DataAccess();
             <div id="sub-title">
                 <i class="material-icons left">live_help</i><h5>拾得物一覧</h5>
             </div>
+            <?php $lostArticle=$access->getLostArticlesList($page,LOST_ARTICLE_PAGE);?>
             <div id="card">
+            <?php   for ($i =0; $i<count($lostArticle);$i++) :?>
                 <div class="lost-article-card">
                     <div class="card-title">
                         <dl>
-                            <dt><h5>拾得物名:ノート</h5></dt>
-                            <dt><p>カテゴリー:文房具</p></dt>
-                            <dd class="card-time"><p>2018/01/25-14:35:00</p></dd>
+                            <dt><h5>拾得物名:<?php  echo $lostArticle[$i]['title']?></h5></dt>
+                            <dt><p>カテゴリー:<?php  echo $lostArticle[$i]['category']?></p></dt>
+                            <dd class="card-time"><p><?php  echo $lostArticle[$i]['datetime']?></p></dd>
                         </dl>
                     </div>
                     <div class="card-content">
-                        182教室に忘れ物20時半までは182教室に置いておきます。<br>
-                        以降は学生カウンターに持って行きます。<br>
+                        <?php  echo $lostArticle[$i]['comment']?>
                     </div>
                     <div class="card-link">
-                        <a href="#">image show</a>
-                    </div>
-                </div>
-                <div class="lost-article-card">
-                    <div class="card-title">
-                        <dl>
-                            <dt><h5>拾得物名:ノート</h5></dt>
-                            <dt><p>カテゴリー:文房具</p></dt>
-                            <dd class="card-time"><p>2018/01/25-14:35:00</p></dd>
-                        </dl>
-                    </div>
-                    <div class="card-content">
-                        182教室に忘れ物20時半までは182教室に置いておきます。<br>
-                        以降は学生カウンターに持って行きます。<br>
-                    </div>
-                    <div class="card-link">
-                        <a href="#">image show</a>
-                    </div>
-                </div>
-                <div class="lost-article-card">
-                    <div class="card-title">
-                        <dl>
-                            <dt><h5>拾得物名:ノート</h5></dt>
-                            <dt><p>カテゴリー:文房具</p></dt>
-                            <dd class="card-time"><p>2018/01/25-14:35:00</p></dd>
-                        </dl>
-                    </div>
-                    <div class="card-content">
-                        182教室に忘れ物20時半までは182教室に置いておきます。<br>
-                        以降は学生カウンターに持って行きます。<br>
-                    </div>
-                    <div class="card-link">
-                        <a href="#">image show</a>
-                    </div>
-                </div>
-                <div class="lost-article-card">
-                    <div class="card-title">
-                        <dl>
-                            <dt><h5>拾得物名:ノート</h5></dt>
-                            <dt><p>カテゴリー:文房具</p></dt>
-                            <dd class="card-time"><p>2018/01/25-14:35:00</p></dd>
-                        </dl>
-                    </div>
-                    <div class="card-content">
-                        182教室に忘れ物20時半までは182教室に置いておきます。<br>
-                        以降は学生カウンターに持って行きます。<br>
-                    </div>
-                    <div class="card-link">
-                        <a href="#">image show</a>
-                    </div>
-                </div>
-                <div class="lost-article-card">
-                    <div class="card-title">
-                        <dl>
-                            <dt><h5>拾得物名:ノート</h5></dt>
-                            <dt><p>カテゴリー:文房具</p></dt>
-                            <dd class="card-time"><p>2018/01/25-14:35:00</p></dd>
-                        </dl>
-                    </div>
-                    <div class="card-content">
-                        182教室に忘れ物20時半までは182教室に置いておきます。<br>
-                        以降は学生カウンターに持って行きます。<br>
-                    </div>
-                    <div class="card-link">
-                        <a href="#">image show</a>
+                        <!-- Modal Trigger -->
+                        <a class="modal-trigger" href="#modal<?php echo $lostArticle[$i]['no']?>">image show</a>
                     </div>
                 </div>
 
+                <!-- Modal Structure -->
+                <div id="modal<?php echo $lostArticle[$i]['no']?>" class="modal modal-fixed-footer">
+                    <div class="modal-content">
+                        <h4><?php  echo $lostArticle[$i]['title']?></h4>
+                        <img src="<?php  echo $lostArticle[$i]['image']?>" alt="<?php  echo $lostArticle[$i]['title']?>">
+                    </div>
+                    <div class="modal-footer">
+                        <a href="#!" class="modal-action modal-close waves-effect waves-green btn-flat ">close</a>
+                    </div>
+                </div>
+            <?php endfor;?>
             </div>
+            <!--  ページング機能  -->
+            <div id="page">
+                <ul class="pagination">
+                    <!--  前を表示させる  -->
+                    <?php if ($page > 1) : ?>
+                        <li class="disabled"><a href="?page=<?php echo $page - 1; ?>"><i class="material-icons">chevron_left</i></a></li>
+                    <?php endif; ?>
+                    <!-- トータル件数が6件より少ない場合はページングを表示させない -->
+                    <?php if($totalPage>LOST_ARTICLE_PAGE): ?>
+                        <?php for ($i=1 ; $i <= $totalPage; $i++): ?><!-- ページングを表示させる処理 -->
+                            <?php if($page == $i) :?>
+                                <li class="active"><a href="?page=<?php echo $i; ?>"><?php echo $i; ?></a></li>
+                            <?php else: ?>
+                                <li class="waves-effect"><a href="?page=<?php echo $i; ?>"><?php echo $i; ?></a></li>
+                            <?php endif; ?>
+                        <?php endfor; ?>
+                    <?php endif; ?>
+                    <!--  次を表示させる  -->
+                    <?php if ($page < $totalPage) : ?>
+                        <li class="waves-effect"><a href="?page=<?php echo $page + 1; ?>"><i class="material-icons">chevron_right</i></a></li>
+                    <?php endif; ?>
+                </ul>
+            </div><!-- page -->
+            <div id="footer">
+                <footer>2017 HAL Students System</footer>
+            </div><!-- footer -->
         </div>
-        <div id="footer">
-            <footer>2017 HAL Students System</footer>
-        </div><!-- footer -->
     </div>
 </div><!-- end wrapper -->
 <script type="text/javascript" src="jq/jquery-3.2.1.min.js"></script>
-<script type="text/javascript" src="js/check.js"></script>
-<script type="text/javascript" src="js/perfect-scrollbar.js"></script>
-<script type="text/javascript" src="js/scrollbar.js"></script>
 <script type="text/javascript" src="js/materialize.min.js"></script>
-<script type="text/javascript" src="js/legacy.js"></script>
-<script type="text/javascript" src="js/lang-ja.js"></script>
-<script type="text/javascript" src="js/app.js"></script>
-<script type="text/javascript" src="js/picker.js"></script>
-<script type="text/javascript" src="js/picker.date.js"></script>
+<script type="text/javascript" src="js/model.js"></script>
+
 </body>
 </html>

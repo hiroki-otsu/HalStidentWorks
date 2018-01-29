e618ce8c1b72392d6ae2557f2a224578e5d8e199
hiroki_ostu <hiroki.ootsu.50054@gmail.com>
2018-01-30 01:33:15 +0900

    教師検索を処理を記述

--- web/teacherSearch.php
+++ web/teacherSearch.php
@@ -5,3 +5,27 @@
  * Date: 2018/01/29
  * Time: 1:01
  */
+//エラー表示
+error_reporting(E_ALL | E_STRICT);
+ini_set('display_errors','On');
+//外部ファイル読み込み
+require '../bootstrap.php';
+//インスタンス化
+$session = new Session();
+$request = new Request();
+$access = new DataAccess();
+$error = new Errors();
+
+$teacher=$request->getGet('teacher');
+$flg=true;
+
+if(empty($teacher)){
+    $flg=false;
+}
+if($flg){
+    $total = $access->getCountDate(TEACHER);
+    $totalPage = ceil($total / TEACHER_PAGE);
+    $access->getTeacher($teacher);
+}else{
+
+}

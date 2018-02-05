3c97651246cf3fffaa5d0edb9d4466b29efd1f69
hiroki_ostu <hiroki.ootsu.50054@gmail.com>
2018-02-05 12:00:57 +0900

    非同期通信する際にデータベースにアクセスするphpファイル

--- web/schedule.php
+++ web/schedule.php
@@ -5,3 +5,24 @@
  * Date: 2018/01/30
  * Time: 20:05
  */
+//エラー表示
+error_reporting(E_ALL | E_STRICT);
+ini_set('display_errors','On');
+//外部ファイル読み込み
+require '../bootstrap.php';
+//インスタンス化
+$request = new Request();
+$access = new DataAccess();
+if ($request->isPost()){
+    $class=$request->getPost('no');
+    if(empty($class)){
+        $error = "格納されていません";
+    }
+    else{
+        $error = "格納されています";
+    }
+    $classNo = str_replace('#', '', $class);
+    $classData=$access->getSchedule($classNo);
+
+    echo json_encode($classData);
+}

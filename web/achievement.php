6ec47d98c71b5f0aadff3e35a2ce24fc38dcd449
hiroki_ostu <hiroki.ootsu.50054@gmail.com>
2018-02-15 14:45:49 +0900

    イベントに参加・不参加の情報を登録するファイル

---
+++ web/achievement.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: hiro
+ * Date: 2018/02/06
+ * Time: 20:32
+ */
---
+++ web/registration.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: hiro
+ * Date: 2018/02/15
+ * Time: 11:32
+ */
